41aef61958d97ff0663e42b9b4eafd9950871b55
kristians-dimitrijevs <kristians.dimitrijevs@gmail.com>
2020-06-21 15:20:02 +0300

    Fixing previous commit issues

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -2,15 +2,11 @@
 
 namespace App\Http\Controllers;
 
-<<<<<<< HEAD
 use App\User;
-=======
->>>>>>> 22763169f28dfa3a7264042b2fe9467d899863cd
 use Illuminate\Http\Request;
 
 class ProfilesController extends Controller
 {
-<<<<<<< HEAD
     public function index($user)
     {
         $user = User::findOrFail($user);
@@ -19,11 +15,4 @@ class ProfilesController extends Controller
             'user' => $user,
         ]);
     }
-=======
-    public function index()
-    {
-        return view('home');
-    }
-
->>>>>>> 22763169f28dfa3a7264042b2fe9467d899863cd
 }
